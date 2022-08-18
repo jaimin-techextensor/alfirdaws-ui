3c2e2fd40d7fc15141ca511b5f55940f7be31611
Khushbu <khushbu.thakkar@techextensor.com>
2022-08-18 16:00:25 +0530

    Add User Changes

--- src/app/modules/users/add-user/add-user.component.ts
+++ src/app/modules/users/add-user/add-user.component.ts
@@ -1,14 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import {Location} from '@angular/common'
 
 @Component({
   selector: 'app-add-user',
   templateUrl: './add-user.component.html'
 })
 export class AddUserComponent implements OnInit {
+  userForm: UntypedFormGroup;
 
-  constructor() { }
+  constructor(private _location: Location,private _formBuilder: UntypedFormBuilder,
+    ) { }
 
   ngOnInit(): void {
+    this.userForm = this._formBuilder.group({
+      firstname : ['', [Validators.required]],
+      lastname : ['', [Validators.required]],
+      email :['', [Validators.required]],
+      username :['', [Validators.required]],
+      password : [''],
+      repetepassword : [''],
+      shouldchangepasswordonnextlogin : [''],
+      sendactivationemail : [''],
+      active : ['']
+    })
   }
-
+  back()
+  {
+    this._location.back();
+  }
 }
